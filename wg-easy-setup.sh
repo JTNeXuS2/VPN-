68486d14441ef8ad3aa9f33838f4a38ded4c5b88
⎛⎝Иллидан⎠⎞ <88918931+JTNeXuS2@users.noreply.github.com>
2026-08-12 01:24:09 +0200

    Update wg-easy-setup.sh

--- wg-easy-setup.sh
+++ wg-easy-setup.sh
@@ -53,6 +53,7 @@ docker run --detach \
   --env PORT=$wg_webport \
   --env WG_CONFIG_PORT=$wg_listen \
   --env WG_PORT=$wg_listen \
+  --env WG_DEFAULT_DNS="9.9.9.9,1.1.1.1,8.8.8.8" \
   --env WG_ALLOWED_IPS="0.0.0.0/1, 128.0.0.0/1, ::/1, 8000::/1" \
   --volume ~/.wg-easy:/etc/wireguard \
   --publish $wg_listen:$wg_listen/udp \
